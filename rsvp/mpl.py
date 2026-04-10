34e3d46d17d831a6b5de7c9a05f0510da23cfe2f
Saskapult <thekieransmith@gmail.com>
2026-04-10 12:16:58 -0700

    small mpl demo

--- rsvp/mpl.py
+++ rsvp/mpl.py
@@ -15,7 +15,7 @@ def resample_and_smooth(x_new, x_old, data) -> np.ndarray:
 	return smoothed
 
 
-def show_plot_frt(xs, ys, sr):
+def show_plot_frt(xs, ys, sr, save_to=None):
 	pause_dur = 1.0 / sr
 
 	plt.ion()
@@ -29,38 +29,34 @@ def show_plot_frt(xs, ys, sr):
 
 	plt.xlim(min(xs)*0.9, max(xs)*1.1)
 	plt.ylim(min(ys)*0.9, max(ys)*1.1)
-	plt.savefig("base_res.png")
-	exit(0)
 
-	savedir = Path("z_thing")
-	savedir.mkdir(exist_ok=True)
+	if save_to:
+		save_to.mkdir(exist_ok=True)
 
-	# the update loop
 	graph = None
 	for i, (xv, yv) in enumerate(zip(xs, ys)):
 		print(f"{i}/{len(xs)}")
 		x.append(xv)
 		y.append(yv)
 		
-		# removing the older graph
 		if graph:
-			graph.remove()
-		
-		# plotting newer graph
+			graph.remove()		
 		graph = plt.plot(x, y, color="red")[0]
 
-		plt.savefig(savedir/f"{i:03d}.png")
-		# plt.pause(pause_dur)
+		if save_to:
+			plt.savefig(save_to/f"{i:03d}.png")
+		else:
+			plt.pause(pause_dur)
 	
-	dramerate = sr/2048
-	print(dramerate)
-	
-	(
-		ffmpeg
-		.input(savedir/"*.png", pattern_type="glob", framerate=dramerate)
-		.output('z_recreate.mp4')
-		.run()
-	)
+	if save_to:
+		dramerate = sr/2048
+		print("Framerate", dramerate)
+		(
+			ffmpeg
+			.input(save_to/"*.png", pattern_type="glob", framerate=dramerate)
+			.output(save_to/'videro.mp4')
+			.run()
+		)
 
 
 def spectrogram_frt(stft, f0, srate):
@@ -110,36 +106,7 @@ def spectrogram_frt(stft, f0, srate):
 	)
 
 
-
-def main():
-	print("Load")
-	x, srate = librosa.load(Path("z_trim.wav"))
-	print("x", len(x))
-	print("srate", srate)
-
-	print("pyin")
-	st = time.time()
-	f0, vf, vp = librosa.pyin(
-		x, sr=srate, 
-		fmin=librosa.note_to_hz('C2'),
-		fmax=librosa.note_to_hz('C7'),
-	)
-	times = librosa.times_like(f0, sr=srate)
-	print("f0", len(f0))
-	print(time.time() - st)
-
-	print("stft")
-	st = time.time()
-	stft = np.abs(librosa.stft(x))
-	stft_freqs = librosa.fft_frequencies(sr=srate)
-	print(time.time() - st)
-
-	# spectrogram_frt(stft, f0, srate)
-	# exit(0)
-
-
-	# For each frame, get peaks 
-	assert len(stft.transpose()[0]) == 1025
+def vocal_resonance(stft, stft_freqs):
 	peaks = [signal.find_peaks(f)[0] for f in stft.transpose()]
 	# For each frame, sort peaks by highest first
 	peak_indices = [
@@ -149,80 +116,129 @@ def main():
 	# For each frame, map those peaks to their frequencies 
 	r = [stft_freqs[p] for p in peak_indices]
 
-	# Compute resonance from first three forants 
-	print("Resonance")
-	r_st = time.time()
 	formants = []
 	for i in range(0, 3):
-		f1 = np.array([d[i] for d in r])
-		f1 = signal.medfilt(f1, kernel_size=7)
-		# f1_peaks = signal.find_peaks(f1)[0]
-		# f1s = resample_and_smooth(times, times[f1_peaks], f1[f1_peaks])
-		f1s = signal.savgol_filter(f1, window_length=17, polyorder=0)
-		formants.append(f1s)
+		fn = np.array([d[i] for d in r])
+		# fn = signal.medfilt(fn, kernel_size=7)
+		# # f1_peaks = signal.find_peaks(f1)[0]
+		# # f1s = resample_and_smooth(times, times[f1_peaks], f1[f1_peaks])
+		# fn_smooth = signal.savgol_filter(fn, window_length=17, polyorder=0)
+		formants.append(fn)
 	# Jess said to do this idk 
 	resonance = np.sum(np.array(formants), axis=0) / len(formants)
-	print(time.time() - r_st)
-
-	# fig, ax = plt.subplots()
-	# ax.set(title="resonance and f0 over time")
-	# D = librosa.amplitude_to_db(stft, ref=np.max)
-	# img = librosa.display.specshow(D, x_axis='time', y_axis='log', ax=ax, sr=srate, alpha=0.5)
-	# fig.colorbar(img, ax=ax, format="%+2.f dB")
-	# ax.plot(times, f0, label='f0', color='cyan', linewidth=3)
-	# for i, f in enumerate(formants):
-	# 	ax.plot(times, f, label=f"f{i+1}", linestyle="--")
-	# ax.plot(times, resonance, label="resonance")
-	# plt.legend()
-	# plt.show()
-
-	print("Weight")
-	w_st = time.time()
-	# slopes = []
-	# for frame in stft.transpose():
-	# 	l = linregress(stft_freqs, frame)
-	# 	slopes.append(l.slope)
-	# slopes = np.array(slopes)
-	# slopes = -slopes
-	# plt.plot(slopes)
-	# plt.show()
-
-	S, phase = librosa.magphase(stft)
-	slopes = librosa.feature.spectral_rolloff(S=S, sr=srate, roll_percent=0.65)[0]
-
-	peaks, _ = signal.find_peaks(slopes)
-	weight = resample_and_smooth(times, times[peaks], slopes[peaks])
-	# plt.plot(times, weight)
-	# plt.show()
-	print(time.time() - w_st)
+	return resonance, formants
 
 
-	# input("Plot?")
+def spectral_slope(stft, stft_freqs):
+	slopes = []
+	for frame in stft.transpose():
+		l = linregress(stft_freqs, frame)
+		slopes.append(l.slope)
+	slopes = np.array(slopes)
+	
+	return slopes
+
+
+def smoothing(x):
+	peaks, _ = signal.find_peaks(x)
+
+	peak_threshold = np.median(x[peaks])
+	# peak_threshold = np.quantile(x[peaks], 0.50)
+	# peaks = peaks[x[peaks] >= peak_threshold]
+
+	x = signal.medfilt(x, kernel_size=7)
+
+	x = resample_and_smooth(np.arange(0, len(x)), np.arange(0, len(x))[peaks], x[peaks])
+
+	x = signal.savgol_filter(x, window_length=17, polyorder=0)
+
+	return x
+
+
+def time_this(f):
+	st = time.time()
+	result = f()
+	en = time.time()
+	return result, en - st
+
+
+def main():
+	# input_mode = input("Input from microphone (m) or clip (c): ")
+	# if input_mode == "c":
+	# 	x, srate = librosa.load(select_file(Path("clips")))
+	# else:
+
+	print("Load")
+	x, srate = librosa.load(Path("clips/z_trim.wav"))
+	print("x", len(x))
+	print("srate", srate)
+
+	stft_freqs = librosa.fft_frequencies(sr=srate)
+
+	stft = np.abs(librosa.stft(x))
+	times = librosa.times_like(stft, sr=srate)
+
+	# f0, vf, vp = librosa.pyin(
+	# 	x, sr=srate, 
+	# 	fmin=librosa.note_to_hz('C2'),
+	# 	fmax=librosa.note_to_hz('C7'),
+	# )
+
+	resonance, formants = vocal_resonance(stft, stft_freqs)
+	
+	if False:
+		fig, ax = plt.subplots(2)
+		# ax.plot(times, f0, label='f0', color='cyan', linewidth=3)
+		for i, f in enumerate(formants):
+			ax[0].plot(times, f, label=f"f{i+1}", linestyle="--")
+		ax[0].plot(times, resonance, label="resonance")
+		ax[0].title.set_text("No smooth")
+
+		r = np.zeros_like(resonance)
+		for i, f in enumerate(formants):
+			s = smoothing(f)
+			r += s
+			ax[1].plot(times, s, label=f"f{i+1}", linestyle="--")
+		r /= 3
+		ax[1].plot(times, smoothing(resonance), label="resonance")
+		ax[1].plot(times, r, label="r")
+		ax[1].title.set_text("Smooth")
+
+		plt.legend()
+		plt.show()
+		exit(0)
+
+	resonance = smoothing(resonance)
+
+	slopes = -spectral_slope(stft, stft_freqs)
+	if False:
+		fig, ax = plt.subplots()
+		ax.plot(times, slopes, label="no smooth")
+
+		ax.plot(times, -smoothing(slopes), label="smooth")
+
+		plt.legend()
+		plt.show()
+		exit(0)
+
+	weight = smoothing(slopes)
+
 
 	print("Plot!")
 	plt.xlabel("Vocal Weight")
 	plt.ylabel("Vocal Resonance")
-	# show_plot_frt(weight[50:-50], resonance[50:-50], srate)
-	# plt.ioff()
-	# plt.show()
+	# plt.plot(weight, resonance, label="Smoothed Interpolated Peaks")	
+	# Clip becuase the ends are always funky
+	# Use windowing in real-time maybe? 
+	cut = 45
+	if False:
+		show_plot_frt(weight[cut:-cut], resonance[cut:-cut], srate)
+		plt.ioff()
+		plt.show()
+	else:
+		plt.plot(weight[cut:-cut], resonance[cut:-cut], label="Smoothed Interpolated Peaks")
+		plt.show()
 
-	print("Done")
-	# plt.plot(weight, resonance)
-
-	
-	plt.plot(weight[50:-50], resonance[50:-50], label="Smoothed Interpolated Peaks")
-	plt.show()
-
-	# plt.legend(loc='upper right')
-	# plt.show()
-	# plt.savefig("vocal_weight.png")
-	# And that's a vocal weight measure! 
-
-	# Make clusters for all data points 
-	# Compare somehow (see bayes lecture)
-
-	# Elaborate on things are done but lacking, is MVP 
-	# Especially the UI 
 
 if __name__ == "__main__":
 	main()
