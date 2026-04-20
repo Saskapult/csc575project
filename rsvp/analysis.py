715a94b49b5c1bbebca3cb4eb02f5ec80f78095d
Saskapult <thekieransmith@gmail.com>
2026-04-19 18:33:28 -0700

    app organization

--- rsvp/analysis.py
+++ rsvp/analysis.py
@@ -12,104 +12,104 @@ from tqdm import tqdm
 import time
 
 
-def show_plot_frt(xs, ys, sr, save_to=None):
-	pause_dur = 1.0 / sr
+# def show_plot_frt(xs, ys, sr, save_to=None):
+# 	pause_dur = 1.0 / sr
 
-	plt.ion()
+# 	plt.ion()
 
-	y = []
-	x = []
+# 	y = []
+# 	x = []
 
-	# plotting the first frame
-	# graph = plt.plot(x,y)[0]
-	# plt.pause(pause_dur)
+# 	# plotting the first frame
+# 	# graph = plt.plot(x,y)[0]
+# 	# plt.pause(pause_dur)
 
-	plt.xlim(min(xs)*0.9, max(xs)*1.1)
-	plt.ylim(min(ys)*0.9, max(ys)*1.1)
+# 	plt.xlim(min(xs)*0.9, max(xs)*1.1)
+# 	plt.ylim(min(ys)*0.9, max(ys)*1.1)
 
-	if save_to:
-		save_to.mkdir(exist_ok=True)
+# 	if save_to:
+# 		save_to.mkdir(exist_ok=True)
 
-	graph = None
-	for i, (xv, yv) in enumerate(zip(xs, ys)):
-		print(f"{i}/{len(xs)}")
-		x.append(xv)
-		y.append(yv)
+# 	graph = None
+# 	for i, (xv, yv) in enumerate(zip(xs, ys)):
+# 		print(f"{i}/{len(xs)}")
+# 		x.append(xv)
+# 		y.append(yv)
 		
-		if graph:
-			graph.remove()		
-		graph = plt.plot(x, y, color="red")[0]
+# 		if graph:
+# 			graph.remove()		
+# 		graph = plt.plot(x, y, color="red")[0]
 
-		if save_to:
-			plt.savefig(save_to/f"{i:03d}.png")
-		else:
-			plt.pause(pause_dur)
+# 		if save_to:
+# 			plt.savefig(save_to/f"{i:03d}.png")
+# 		else:
+# 			plt.pause(pause_dur)
 	
-	if save_to:
-		dramerate = sr/2048
-		print("Framerate", dramerate)
-		(
-			ffmpeg
-			.input(save_to/"*.png", pattern_type="glob", framerate=dramerate)
-			.output(save_to/'videro.mp4')
-			.run()
-		)
+# 	if save_to:
+# 		dramerate = sr/2048
+# 		print("Framerate", dramerate)
+# 		(
+# 			ffmpeg
+# 			.input(save_to/"*.png", pattern_type="glob", framerate=dramerate)
+# 			.output(save_to/'videro.mp4')
+# 			.run()
+# 		)
 
 
-def spectrogram_frt(stft, f0, srate):
-	times = librosa.times_like(f0, sr=srate)
+# def spectrogram_frt(stft, f0, srate):
+# 	times = librosa.times_like(f0, sr=srate)
 
-	# Spectrogram
-	D = librosa.amplitude_to_db(stft, ref=np.max)
+# 	# Spectrogram
+# 	D = librosa.amplitude_to_db(stft, ref=np.max)
 	
 	
-	fig, ax = plt.subplots()
-	ax.set(title='pYIN fundamental frequency estimation')
-	img = librosa.display.specshow(D, x_axis='time', y_axis='log', ax=ax, sr=srate, alpha=0.5)
-	fig.colorbar(img, ax=ax, format="%+2.f dB")
-	ax.plot(times, f0, label='f0', color='cyan', linewidth=3)
+# 	fig, ax = plt.subplots()
+# 	ax.set(title='pYIN fundamental frequency estimation')
+# 	img = librosa.display.specshow(D, x_axis='time', y_axis='log', ax=ax, sr=srate, alpha=0.5)
+# 	fig.colorbar(img, ax=ax, format="%+2.f dB")
+# 	ax.plot(times, f0, label='f0', color='cyan', linewidth=3)
 
-	winsize=100
+# 	winsize=100
 
-	# plt.plot(times, f0)
+# 	# plt.plot(times, f0)
 
-	savedir = Path("spectrogram")
-	savedir.mkdir(exist_ok=True)
+# 	savedir = Path("spectrogram")
+# 	savedir.mkdir(exist_ok=True)
 
-	plt.ion()
-	window_dur = times[winsize] - times[0]
-	print("wdur", window_dur)
-	for i in tqdm(range(0, len(f0)-winsize, 1)):
-		# removing the older graph
-		# if graph:
-		# 	graph.remove()
+# 	plt.ion()
+# 	window_dur = times[winsize] - times[0]
+# 	print("wdur", window_dur)
+# 	for i in tqdm(range(0, len(f0)-winsize, 1)):
+# 		# removing the older graph
+# 		# if graph:
+# 		# 	graph.remove()
 		
-		# plotting newer graph
-		# After past winsize, then move it 
-		t = times[i:i+winsize]
-		# graph = plt.plot(t, f0[max(0, i-winsize):i], color="red")[0]
-		plt.xlim(t[0]-window_dur, t[-1]-window_dur)
+# 		# plotting newer graph
+# 		# After past winsize, then move it 
+# 		t = times[i:i+winsize]
+# 		# graph = plt.plot(t, f0[max(0, i-winsize):i], color="red")[0]
+# 		plt.xlim(t[0]-window_dur, t[-1]-window_dur)
 
-		plt.savefig(savedir/f"{i:03d}.png")
+# 		plt.savefig(savedir/f"{i:03d}.png")
 	
-	dramerate = srate*4/2048
-	print(dramerate)
+# 	dramerate = srate*4/2048
+# 	print(dramerate)
 	
-	(
-		ffmpeg
-		.input(savedir/"*.png", pattern_type="glob", framerate=dramerate)
-		.output('spectrogram_timed.mp4')
-		.run()
-	)
+# 	(
+# 		ffmpeg
+# 		.input(savedir/"*.png", pattern_type="glob", framerate=dramerate)
+# 		.output('spectrogram_timed.mp4')
+# 		.run()
+# 	)
 
-def vr_1d(frame_fft, fft_freqs):
-	peaks = signal.find_peaks(frame_fft)[0]
-	peak_indices = np.argsort(frame_fft[peaks])[::-1]
-	stft_peak_frequencies = fft_freqs[peak_indices]
+# def vr_1d(frame_fft, fft_freqs):
+# 	peaks = signal.find_peaks(frame_fft)[0]
+# 	peak_indices = np.argsort(frame_fft[peaks])[::-1]
+# 	stft_peak_frequencies = fft_freqs[peak_indices]
 	
-	formants = np.array(stft_peak_frequencies[:3])
-	resonance = np.mean(np.array(formants), axis=0)
-	return resonance, formants
+# 	formants = np.array(stft_peak_frequencies[:3])
+# 	resonance = np.mean(np.array(formants), axis=0)
+# 	return resonance, formants
 
 
 def vocal_resonance(stft, stft_freqs):
@@ -122,53 +122,19 @@ def vocal_resonance(stft, stft_freqs):
 	# For each frame, map those peaks to their frequencies 
 	stft_peak_frequencies = [stft_freqs[p] for p in stft_peak_indices]
 
-	# # Could improve efficiency by finding first three max 
-	# # Apparently argpartition can do this 
-	# # stft_peaks_2 = np.argsort(stft.transpose(), axis=1)[:,::-1][:,:3]
-	# stft_peaks_2 = np.array([signal.find_peaks(frame)[0] for frame in stft.transpose()])
-	# stft_peak_indices_2 = np.argsort(stft.transpose()[stft_peaks_2], axis=1)[:,::-1]
-	# # print(stft_peaks_2)
-	# # print("new", stft_peaks_2[0])
-	# # print("old", stft_peaks[0])
-	# # exit(0)
-	# print(stft_freqs.shape)
-	# print(stft.shape)
-	# print(stft_peaks_2.shape)
-	# print("x", stft_peaks_2[0].shape)
-	# print("y", stft.transpose()[0].shape)
-	# i = 42
-	# plt.plot(stft.transpose()[i])
-	# # plt.plot(stft_peaks_2[i], stft.transpose()[i][stft_peaks_2[i]], marker="x", label="2")
-	# plt.plot(stft_peak_indices_2[i][:3], stft.transpose()[i][stft_peak_indices_2[i][:3]], marker="x", label="2")
-	# plt.plot(stft_peak_indices[i][:3], stft.transpose()[i][stft_peak_indices[i][:3]], marker="x", label="1")
-	# plt.legend()
-	# plt.show()
-	# exit(0)
-
-	# print("stft", stft)
-	# print(f"p {stft_peaks}")
-	# print(f"pi {stft_peak_indices}")
-	# print("pf", stft_peak_frequencies)
-
 	formants = []
 	for i in range(0, 3):
 		# For each sample, map to the i-th formant frequency 
-		# print(stft_peak_frequencies)
-		# print(stft_peak_frequencies)
 		fn = np.array([d[i] if len(d) > i else 0.0 for d in stft_peak_frequencies])
-		# fn = signal.medfilt(fn, kernel_size=7)
-		# # f1_peaks = signal.find_peaks(f1)[0]
-		# # f1s = resample_and_smooth(times, times[f1_peaks], f1[f1_peaks])
-		# fn_smooth = signal.savgol_filter(fn, window_length=17, polyorder=0)
 		formants.append(fn)
-	# Jess said to do this idk 
-	# print("formants", formants)
-	# What if we weighted by their amplitudes? 
+	
+	# Compute the mean of the 3 formant frequencies 
 	resonance = np.mean(np.array(formants), axis=0)
 	return resonance, formants
 
 
 def spectral_slope(stft, stft_freqs):
+	# For each stft decomposition, fit a line to its values 
 	slopes = []
 	for frame in stft.transpose():
 		l = linregress(stft_freqs, frame)
@@ -178,41 +144,8 @@ def spectral_slope(stft, stft_freqs):
 	return slopes
 
 
-def smoothing(x):
-	x = signal.medfilt(x, kernel_size=7)
-	# x = signal.savgol_filter(x, window_length=17, polyorder=0)
-
-	peaks, _ = signal.find_peaks(x)
-	proms, _, _ = signal.peak_prominences(x, peaks)
-	# print(peaks.shape)
-	# print(proms.shape)
-
-	# prom_threshold = np.mean(proms)
-	# peaks = peaks[proms >= prom_threshold]
-
-	# peak_threshold = np.median(x[peaks])
-	# peak_threshold = np.quantile(x[peaks], 0.50)
-	# peak_threshold = np.mean(x[peaks])
-	# peaks = peaks[x[peaks] >= peak_threshold]
-
-	# x = signal.medfilt(x, kernel_size=7)
-
-	x = np.interp(np.arange(0, len(x)), np.arange(0, len(x))[peaks], x[peaks])
-	x = uniform_filter1d(x, size=41)
-
-	# Inflate for filter 
-	# x = np.interp(np.arange(0, len(x)*2), np.arange(0, len(x))[peaks], x[peaks])
-
-	# x = signal.savgol_filter(x, window_length=17, polyorder=0)
-
-	# x = np.interp(np.arange(0, len(x)//2), np.arange(0, len(x)), x)
-
-	# x = signal.medfilt(x, kernel_size=69)
-
-	return x
-
-
 def time_this(f):
+	""" Measure the amount of time taken to exectute a function. """
 	st = time.time()
 	result = f()
 	en = time.time()
@@ -281,21 +214,8 @@ def plot_progressive(xs, srate, n_fft=1024):
 	plt.show()
 
 
-def scatter(files):
-	results = []
-	for file in tqdm(files):
-		x, srate = librosa.load(file)
-
-		stft_freqs = librosa.fft_frequencies(sr=srate, n_fft=512)
-		stft = np.abs(librosa.stft(x, n_fft=512))
-
-		resonance, _ = vocal_resonance(stft, stft_freqs)
-		slopes = -spectral_slope(stft, stft_freqs)
-
-		results.append((np.mean(resonance), np.mean(slopes)))
-
-
 def cache_or(path, f, overwrite=False):
+	""" Loads a value from a path or computes it using a function. """
 	if overwrite or not path.exists():
 		data = f()
 		with open(path, "w") as fp:
@@ -306,7 +226,11 @@ def cache_or(path, f, overwrite=False):
 	return data
 
 
-def gender_scatter(n_fft, limit=None, overwrite=False):
+def metrics_by_gender(n_fft, limit=None):
+	"""
+	Compute metrics for each voice clip in the `./data/f` and `./data/m` directories. 
+	"""
+
 	if limit is None:
 		limit = 99999999999
 	ffiles = list(Path("data/f").iterdir())[:limit//2]
@@ -342,20 +266,59 @@ def gender_scatter(n_fft, limit=None, overwrite=False):
 
 	return data
 
-def gs(n_fft):
-	limit = 80
+
+def gender_scatter(n_fft, limit=80):
+	""" 
+	Creates a scatter plot showing mean vocal weight and resonance for 
+	speakers divided by gender. 
+	"""
+
 	# Make data or load cache 
-	cache_file = Path(f"clips/anacache22_{n_fft}_{limit}.json")
+	cache_file = Path(f"clips/gender_scatter_{n_fft}_{limit}.json")
 	# cache_or
-	data ={}
+	data = cache_or(cache_file, lambda: metrics_by_gender(n_fft, limit=limit))
 
 	for t, d in data.items():
 		colour = "red" if t == "f" else "blue"
 		x = [np.mean(r) for r in d["weight"]]
 		y = [np.mean(w) for w in d["resonance"]]
 		plt.scatter(x, y, label=t, color=colour)
-	plt.show()
 
+
+
+
+def smoothing(x):
+	x = signal.medfilt(x, kernel_size=7)
+	# x = signal.savgol_filter(x, window_length=17, polyorder=0)
+
+	peaks, _ = signal.find_peaks(x)
+	proms, _, _ = signal.peak_prominences(x, peaks)
+	# print(peaks.shape)
+	# print(proms.shape)
+
+	# prom_threshold = np.mean(proms)
+	# peaks = peaks[proms >= prom_threshold]
+
+	# peak_threshold = np.median(x[peaks])
+	# peak_threshold = np.quantile(x[peaks], 0.50)
+	# peak_threshold = np.mean(x[peaks])
+	# peaks = peaks[x[peaks] >= peak_threshold]
+
+	# x = signal.medfilt(x, kernel_size=7)
+
+	x = np.interp(np.arange(0, len(x)), np.arange(0, len(x))[peaks], x[peaks])
+	x = uniform_filter1d(x, size=41)
+
+	# Inflate for filter 
+	# x = np.interp(np.arange(0, len(x)*2), np.arange(0, len(x))[peaks], x[peaks])
+
+	# x = signal.savgol_filter(x, window_length=17, polyorder=0)
+
+	# x = np.interp(np.arange(0, len(x)//2), np.arange(0, len(x)), x)
+
+	# x = signal.medfilt(x, kernel_size=69)
+
+	return x
 
 def main():
 
@@ -363,8 +326,8 @@ def main():
 	# n_fft = 1024
 	n_fft = 2048
 
-	# gender_scatter(512)
-	# exit(0)
+	# gender_scatter(n_fft)
+	# plt.show()
 
 	print("Load")
 	x, srate = librosa.load(Path("clips/z_trim.wav"))
--- rsvp/app.py
+++ rsvp/app.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import sys
 import time
 import librosa
 import numpy as np
@@ -6,7 +7,9 @@ import matplotlib.pyplot as plt
 import pyaudio
 from threading import Lock
 from scipy import signal
-from mpl import cache_or, gender_scatter, smoothing, spectral_slope, vocal_resonance
+from analysis import cache_or, metrics_by_gender, spectral_slope, vocal_resonance
+from pydub import AudioSegment
+import simpleaudio
 
 
 class VoiceApp:
@@ -53,7 +56,7 @@ class VoiceApp:
 		self.slope_segments.append(slopes)
 		self.lock.release()
 	
-	def plot(self, limit=10):
+	def plot(self, limit=100):
 		self.lock.acquire()
 		resonance = np.hstack(self.resonance_segments[-limit:])
 		slopes = np.hstack(self.slope_segments[-limit:])
@@ -65,98 +68,33 @@ class VoiceApp:
 			graph.remove()
 		self.graphs.clear()
 
-
-		# Windows 
-		# Averages? 
-		# Convolve 
-		# ?
-
-
-		# resonance = np.convolve(resonance, np.ones(7), "valid")
-		# slopes = np.convolve(slopes, np.ones(7), "valid")
 		resonance = signal.medfilt(resonance, kernel_size=5)
 		slopes = signal.medfilt(slopes, kernel_size=5)
 
-		filtsz = 15000000000000
 		peak_d = 25
 
 		self.graphs.append(self.ax[0].plot(resonance, color="red", label="resonance")[0])
 		res_peaks = signal.find_peaks(resonance, distance=peak_d)[0]
 		self.graphs.append(self.ax[0].plot(res_peaks, resonance[res_peaks], color="red", marker="x")[0])
 		res_peaks_mf = resonance[res_peaks]#signal.medfilt(resonance[res_peaks], kernel_size=3)
-		# self.graphs.append(self.ax[0].plot(res_peaks, res_peaks_mf, color="blue", marker="x", label="smoothed")[0])
 		res_peaks_mf_i = np.interp(
 			np.arange(0, len(resonance)),
 			res_peaks,
 			res_peaks_mf,
-		)
-		# if len(res_peaks_mf_i) >= filtsz:
-			# res_peaks_mf_i = signal.medfilt(res_peaks_mf_i, filtsz)
-		# res_peaks_mf_i = np.convolve(resonance, np.ones(71), "valid") / 71
-		# self.graphs.append(self.ax[0].plot(res_peaks_mf_i, color="blue", label="smoothed")[0])
+		) if len(res_peaks) > 0 else np.array([])
 		self.ax[0].legend(loc="upper left")
 	
 		self.graphs.append(self.ax[1].plot(slopes, color="red", label="weight")[0])
 		wei_peaks = signal.find_peaks(slopes, distance=peak_d)[0]
 		self.graphs.append(self.ax[1].plot(wei_peaks, slopes[wei_peaks], color="red", marker="x")[0])
 		wei_peaks_mf = signal.medfilt(slopes[wei_peaks], kernel_size=3)
-		# self.graphs.append(self.ax[1].plot(wei_peaks, wei_peaks_mf, color="blue", marker="x", label="smoothed")[0])
 		wei_peaks_mf_i = np.interp(
 			np.arange(0, len(slopes)),
 			wei_peaks,
 			wei_peaks_mf,
-		)
-		# if len(wei_peaks_mf_i) >= filtsz:
-			# wei_peaks_mf_i = signal.medfilt(wei_peaks_mf_i, filtsz)
-		# wei_peaks_mf_i = np.convolve(slopes, np.ones(71), "valid") / 71
-		# self.graphs.append(self.ax[1].plot(wei_peaks_mf_i, color="blue", label="smoothed")[0])
+		) if len(wei_peaks) > 0 else np.array([])
 		self.ax[1].legend(loc="upper left")
 
-
-		# self.graphs.append(self.ax.axhline(np.mean(resonance), color="blue", label="mean"))
-		# self.graphs.append(self.ax.axhline(np.median(resonance), color="green", label="median"))
-
-		# med = np.median(resonance)
-		# samples_i = np.arange(0, len(resonance))[resonance > med]
-		# interp = np.interp(np.arange(0, len(resonance)), samples_i, resonance[samples_i])
-
-		# chunk_size = 7
-		# thing = np.array_split(resonance, len(resonance) // chunk_size)
-		# print(thing)
-		# print([len(t) for t in thing])
-		# thing = thing[:-1]
-		# print(thing)
-		# print([len(t) for t in thing])
-		# exit(0)
-		# # thing_vals = np.mean(thing, axis=1)
-		# thing_vals = np.mean(thing, axis=1)
-		# thing_indices = np.arange(0, len(resonance), chunk_size)
-
-		# r_means = np.max(self.resonance_segments[-limit:], axis=1)
-		# r_means = signal.medfilt(r_means, kernel_size=7)
-		# print("moree", len(self.resonance_segments[-limit:]))
-		# print("basee", resonance.shape)
-		# print("means", r_means.shape)
-		# r_mean_smaples = np.hstack(r_means)
-		# print("stacked", r_mean_smaples)
-		# print(len(self.resonance_segments[-limit:]))
-		# interp = np.interp(
-		# 	np.arange(0, len(resonance)), 
-		# 	np.arange(0, len(self.resonance_segments[-limit:]))*2, 
-		# 	r_means,
-		# )
-
-		# print(interp)
-		# self.graphs.append(self.ax.plot(
-			# thing_vals, color="purple", label="interp", linestyle="--")[0])
-
-		# self.graphs.append(self.ax.plot(slopes, color="red")[0])
-		# graph = self.ax.plot(smoothing(resonance), color="red")[0]
-		# graph = plt.plot(smoothing(slopes), color="red")[0]
-		# self.graph = plt.plot(smoothing(slopes), smoothing(resonance), color="red")[0]
-		# self.graph = self.ax.plot(np.mean(slopes), np.mean(resonance), color="red", marker="x")[0]
-		# self.graph = self.ax.plot(np.max(slopes), np.max(resonance), color="red", marker="x")[0]
-		# to_i = min(np.max(res_peaks), np.max(wei_peaks))
 		to_i = min(
 			res_peaks[-2] if len(res_peaks) >= 2 else 0,
 			wei_peaks[-2] if len(wei_peaks) >= 2 else 0,
@@ -168,12 +106,8 @@ class VoiceApp:
 		self.ax[2].set_xlabel("Vocal Weight")
 		self.ax[2].set_ylabel("Vocal Resonance")
 
-		# plt.pause(0.001)
-		# plt.show()
-
-		# self.ax.set_xbound(self.wmin, self.wmax)
-		# self.ax.set_ybound(self.rmin, self.rmax)
-		# self.ax.legend(loc="upper right")
+		self.ax[2].set_xbound(self.wmin, self.wmax)
+		self.ax[2].set_ybound(self.rmin, self.rmax)
 
 		def on_close(_event):
 			if not self.closed:
@@ -183,7 +117,7 @@ class VoiceApp:
 		self.fig.canvas.mpl_connect('close_event', on_close)
 
 
-def start_recording():
+def run_rt():
 	srate = 22050
 	app = VoiceApp(srate)
 
@@ -234,18 +168,29 @@ def plot_file(path):
 	time_per_iteration = intended_duration / n_iterations
 	print(f"Each iteration takes {time_per_iteration*1000}ms")
 
+	# Play the clip in the background
+	seg = AudioSegment.from_wav(path)
+	playback = simpleaudio.play_buffer(
+		seg.raw_data, 
+		num_channels=seg.channels, 
+		bytes_per_sample=seg.sample_width, 
+		sample_rate=seg.frame_rate
+	)
+
 	plt.ion()
 	st = time.time()
 	for i, o in enumerate(range(0, len(x), app.segment_size)):
 		t_iteration_st = time.time()
 		app.update(x[o:o+app.segment_size])
-		if i > 16:
+		if i > 16 and i % 8 == 0:
 			app.plot(limit=5000000)
 		t_iteration_end = time.time()
 
 		iteration_dur = t_iteration_end - t_iteration_st
 		print(f"iteration took {iteration_dur*1000:.2f}ms ({iteration_dur/time_per_iteration*100:.2f}% of intended)")
 
+		# Discern where the program should be at this given time. 
+		# Wait for the appropreate amount of time before continuing. 
 		intended_next_st = st + (i+1) * time_per_iteration
 		pause_dur = max(0.0000000001, intended_next_st - t_iteration_end)
 		print("Pause", pause_dur)
@@ -256,7 +201,8 @@ def plot_file(path):
 			break
 	en = time.time()
 	print(f"Played {en-st}s ({(en-st)/intended_duration*100:.2f}% of intended)")
-	
+	playback.stop()
+
 	plt.ioff()
 	plt.show()
 
@@ -279,7 +225,7 @@ def calibrate(n_fft, overwrite=False, limit=40):
 	calibate_cache_path = Path(f"clips/app_calibration_{n_fft}_{limit}.json")
 	data = cache_or(
 		calibate_cache_path, 
-		lambda: gender_scatter(n_fft, limit=limit),
+		lambda: metrics_by_gender(n_fft, limit=limit),
 		overwrite=overwrite,
 	)
 
@@ -296,15 +242,32 @@ def calibrate(n_fft, overwrite=False, limit=40):
 	
 	return min_resonance, max_resonance, min_weight, max_weight
 
+def run_clip(file=None):
+	if file is None:
+		file = select_file(Path("./clips"))
+	else:
+		file = Path(file)
+	plot_file(file)
+
 
 def main():
-	# if input("Interactive (i) or clip (c): ") == "i":
-	# 	if input("Microphone (m) or clip (c): ") == "m":
-	# 		print("TODO: mic clipped shit")
-	# print("Clipped stuff pls")
+	if sys.argv[1] == "rt":
+		run_rt()
+	elif sys.argv[1] == "clip":
+		if len(sys.argv) > 2:
+			run_clip(sys.argv[2])
+		else:
+			run_clip()
+	else:
+		print("Enter 'rt' for real-time mic input or 'clip' to analyze a clip")
+		exit(1)
+	# # if input("Interactive (i) or clip (c): ") == "i":
+	# # 	if input("Microphone (m) or clip (c): ") == "m":
+	# # 		print("TODO: mic clipped shit")
+	# # print("Clipped stuff pls")
 
-	# start_recording()
-	plot_file(Path("clips/z_trim.wav"))
+	# # run_rt()
+	# plot_file(Path("clips/z_trim.wav"))
 
 
 
